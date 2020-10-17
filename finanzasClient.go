507489be88803d5f1a304d358578ec0fdd52b2ad
Fralkayg <csepulvedav@outlook.com>
2020-10-17 18:11:34 -0300

    no se como funciona esto

--- finanzasClient.go
+++ finanzasClient.go
@@ -90,19 +90,75 @@ func registrarFinanza(paquete infoPaquete){
 	// csvWriter.Flush()
 }
 
-func main(){
-	//conn con rabbit xd
-	// usar colas rabbitMQ
-	// La representacion de los datos enviados a la cola de mensajes del sistema financiero debe ser mediante JSON.
-	// se obtiene un paquetito 
-	var balance float32
+// func main(){
+// 	//conn con rabbit xd
+// 	// usar colas rabbitMQ
+// 	// La representacion de los datos enviados a la cola de mensajes del sistema financiero debe ser mediante JSON.
+// 	// se obtiene un paquetito 
+// 	var balance float32
 
-	for{ 
-		ingresos = ingresoPaquete(paquetito)
-		registrarFinanza(paquetito)
+// 	for{ 
+// 		ingresos = ingresoPaquete(paquetito)
+// 		registrarFinanza(paquetito)
 
-		balance += ingresos
-		log.Printf("Balance: %f dignipesos",balance)
+// 		balance += ingresos
+// 		log.Printf("Balance: %f dignipesos",balance)
+// 	}
+
+// }
+
+
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
 	}
+}
 
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+
+			// ingresos = ingresoPaquete(paquetito)
+			// registrarFinanza(paquetito)
+
+			// balance += ingresos
+			// log.Printf("Balance: %f dignipesos",balance)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
 }
--- logisticaServer.go
+++ logisticaServer.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	pb "github.com/Fralkayg/sd-t1/Service"
+	"github.com/streadway/amqp"
 	"google.golang.org/grpc"
 )
 
@@ -55,6 +56,12 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
 }
 
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
 func (s *server) ActualizarSeguimiento(ctx context.Context, updateSeguimiento *pb.UpdateSeguimiento) (*pb.StatusSeguimiento, error) {
 	for s.lock {
 	}
@@ -80,6 +87,36 @@ func (s *server) ActualizarSeguimiento(ctx context.Context, updateSeguimiento *p
 	s.seguimientoPaquetes[index].Intentos = int(updateSeguimiento.Intentos)
 
 	fmt.Println("Seguimiento: ", s.seguimientoPaquetes)
+
+	conn, err := amqp.Dial("amqp://guest:guest@dist56:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body := "Hello World!"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
 
 	s.lock = false
 
