edfe67901591b3992e356ae68cad20018e1dcdd3
Fralkayg <csepulvedav@outlook.com>
2020-10-17 20:34:24 -0300

    makefile

---
+++ makefile.mk
@@ -0,0 +1,10 @@
+logistica:
+    /sbin/service rabbitmq-server start
+    go run logisticaService.go
+finanzas:
+    /sbin/service rabbitmq-server start
+    go run finanzasClient.go
+cliente:
+    go run clientesClient.go
+camion:
+    go run camionClient.go
